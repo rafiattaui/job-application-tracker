ba81130a8d8c0fa9220de9b27ae122cfeb4ff090
rafiattaui <athallah.rafi13@gmail.com>
2026-02-20 14:36:24 +0700

    copied models.

--- lib/models/column.ts
+++ lib/models/column.ts
@@ -1,37 +1,44 @@
-import mongoose, { Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IColumn extends Document {
-    name: string;
-    boardId: mongoose.Types.ObjectId;
-    order: number;
-    jobApplications: mongoose.Types.ObjectId[];
-    createdAt: Date;
-    updatedAt: Date;
+  name: string;
+  boardId: mongoose.Types.ObjectId;
+  order: number;
+  jobApplications: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
+// Board -> Columns -> JobApplications
+
 const ColumnSchema = new Schema<IColumn>(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        boardId: {
-            type: Schema.Types.ObjectId,
-            ref: "Board",
-            required: true,
-            index: true
-        },
-        order: {
-            type: Number,
-            required: true,
-            default: 0
-        },
-        jobApplications: [{
-            type: Schema.Types.ObjectId,
-            ref: "Column"
-        }]
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
-)
+    boardId: {
+      type: Schema.Types.ObjectId,
+      ref: "Board",
+      required: true,
+      index: true,
+    },
+    order: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    jobApplications: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "JobApplication",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Column ||
+  mongoose.model<IColumn>("Column", ColumnSchema);
